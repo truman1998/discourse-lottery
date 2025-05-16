892a75c43a1994d2b625ccc95e1e1db5c1800573
truman1998 <68187484+truman1998@users.noreply.github.com>
2025-05-16 17:45:02 +0800

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -1,17 +1,19 @@
 # name: discourse-lottery
 # about: 一个在 Discourse 帖子中创建抽奖的插件。
-# version: 1.0.3
+# version: 1.0.4
 # authors: 您的名字 (例如, Truman)
 # url: https://github.com/truman1998/discourse-lottery
 # required_version: 2.8.0.beta10
 
-enabled_site_setting :lottery_enabled # 启用抽奖插件的站点设置
+# Rails.logger.info "LotteryPlugin: plugin.rb TOP LEVEL - File is being loaded."
+
+enabled_site_setting :lottery_enabled
 
 register_asset "stylesheets/common/lottery.scss"
 
 after_initialize do
-  begin # 添加 begin-rescue 块来捕获初始化期间的潜在错误
-    # 定义插件的 Rails 引擎
+  Rails.logger.info "LotteryPlugin: START of after_initialize block."
+  begin
     module ::LotteryPlugin
       PLUGIN_NAME ||= "discourse-lottery".freeze
 
@@ -20,46 +22,46 @@ after_initialize do
         isolate_namespace LotteryPlugin
       end
     end
+    Rails.logger.info "LotteryPlugin: Engine defined."
 
-    # 加载模型文件
     require_relative "app/models/lottery_plugin/lottery"
     require_relative "app/models/lottery_plugin/lottery_entry"
+    Rails.logger.info "LotteryPlugin: Models loaded."
 
-    # 加载库文件
     require_relative "lib/lottery_plugin/parser"
+    Rails.logger.info "LotteryPlugin: Parser lib loaded."
 
-    # 处理帖子的事件监听器
     DiscourseEvent.on(:post_process_cooked) do |doc, post|
       begin
         if SiteSetting.lottery_enabled && post && doc
-          # Rails.logger.info "LotteryPlugin: [Event :post_process_cooked] Processing post ID #{post.id}."
+          # Rails.logger.debug "LotteryPlugin: [Event :post_process_cooked] Processing post ID #{post.id}."
           LotteryPlugin::Parser.parse(post, doc)
         end
       rescue => e
         Rails.logger.error "LotteryPlugin: [Event :post_process_cooked] Error processing post ID #{post&.id}. Error: #{e.class.name} - #{e.message}\nBacktrace: #{e.backtrace.take(10).join("\n")}"
       end
     end
+    Rails.logger.info "LotteryPlugin: :post_process_cooked event handler registered."
 
-    # 加载控制器文件
     require_dependency File.expand_path("../app/controllers/lottery_plugin/entries_controller.rb", __FILE__)
+    Rails.logger.info "LotteryPlugin: EntriesController loaded."
 
-    # 定义插件路由
     LotteryPlugin::Engine.routes.draw do
       post "/entries" => "entries#create"
     end
+    Rails.logger.info "LotteryPlugin: Engine routes drawn."
 
-    # 将插件的引擎挂载到主 Discourse 应用的路由中
     Discourse::Application.routes.append do
       mount ::LotteryPlugin::Engine, at: "/lottery_plugin"
     end
+    Rails.logger.info "LotteryPlugin: Engine mounted into Application routes."
 
-    # 向帖子序列化器添加数据
     add_to_serializer(:post, :lottery_data, false) do
       begin
-        # Rails.logger.info "LotteryPlugin Serializer: Checking post ID #{object.id}"
+        # Rails.logger.debug "LotteryPlugin Serializer: Checking post ID #{object.id}"
         lottery = LotteryPlugin::Lottery.find_by(post_id: object.id)
         if lottery
-          # Rails.logger.info "LotteryPlugin Serializer: Found lottery ##{lottery.id} for post #{object.id}"
+          # Rails.logger.debug "LotteryPlugin Serializer: Found lottery ##{lottery.id} for post #{object.id}"
           {
             id: lottery.id,
             title: lottery.title,
@@ -69,15 +71,19 @@ after_initialize do
             total_entries: lottery.entries.count,
           }
         else
-          # Rails.logger.info "LotteryPlugin Serializer: No lottery found for post #{object.id}"
           nil
         end
       rescue => e
         Rails.logger.error "LotteryPlugin Serializer: Error for post ID #{object&.id}. Error: #{e.class.name} - #{e.message}\nBacktrace: #{e.backtrace.take(10).join("\n")}"
-        nil # 在序列化器中出错时返回 nil，以避免破坏整个帖子流
+        nil
       end
     end
-  rescue => e # 捕获 after_initialize 块中的任何错误
-    Rails.logger.error "LotteryPlugin: FATAL ERROR during after_initialize. Plugin may not be fully functional. Error: #{e.class.name} - #{e.message}\nBacktrace: #{e.backtrace.join("\n")}"
+    Rails.logger.info "LotteryPlugin: Serializer for :post added."
+
+  rescue => e
+    Rails.logger.error "LotteryPlugin: FATAL ERROR during after_initialize. Error: #{e.class.name} - #{e.message}\nBacktrace:\n#{e.backtrace.join("\n")}"
   end
+  Rails.logger.info "LotteryPlugin: END of after_initialize block."
 end
+
+# Rails.logger.info "LotteryPlugin: plugin.rb BOTTOM LEVEL - File loading complete."
